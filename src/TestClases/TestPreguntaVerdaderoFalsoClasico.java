3623bef9b7f4c5d9ab62211d036ce2a1e0bea0b9
SebastianLoe1 <sloe@fi.uba.ar>
2024-06-02 17:27:28 -0300

    Agregado test en TestPreguntaVerdaderoFalsoClasico

--- src/TestClases/TestPreguntaVerdaderoFalsoClasico.java
+++ src/TestClases/TestPreguntaVerdaderoFalsoClasico.java
@@ -1,16 +1,37 @@
 package TestClases;
 
 import main.PreguntaVerdaderoFalsoClasico;
+import main.Respuesta;
+import main.RespuestaCorrectaVerdaderoFalsoClasico;
+import main.RespuestaIncorrectaVerdaderoFalsoClasico;
 
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestPreguntaVerdaderoFalsoClasico {
     @org.junit.jupiter.api.Test
-    public void Test01UnaPreguntaSeInicializaCorrectamente() {
+    public void Test01UnaPreguntaSeInicializaCorrectamenteConSusContenidos() {
 
-        Boolean respuestaEsperada = true;
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+
+        RespuestaCorrectaVerdaderoFalsoClasico respuesta1 = new RespuestaCorrectaVerdaderoFalsoClasico("Verdadero", true);
+        RespuestaIncorrectaVerdaderoFalsoClasico respuesta2 = new RespuestaIncorrectaVerdaderoFalsoClasico("Falso", false);
+
+        respuestas.add(respuesta1);
+        respuestas.add(respuesta2);
+
+        String enunciado = "Pregunta de Ejemplo";
+
+        String enunciadoEsperado = enunciado;
+        ArrayList<Respuesta> respuestasEsperadas = respuestas;
+
+        PreguntaVerdaderoFalsoClasico pregunta = new PreguntaVerdaderoFalsoClasico(enunciado, respuestas);
+
+        assertEquals(enunciadoEsperado, pregunta.getEnunciado());
+        assertEquals(respuestasEsperadas, pregunta.respuestasPosibles());
 
 
     }
--- src/main/PreguntaVerdaderoFalsoClasico.java
+++ src/main/PreguntaVerdaderoFalsoClasico.java
@@ -34,4 +34,8 @@ public class PreguntaVerdaderoFalsoClasico implements Pregunta {
         return respuestas;
     }
 
+    public String getEnunciado() {
+        return enunciado;
+    }
+
 }
