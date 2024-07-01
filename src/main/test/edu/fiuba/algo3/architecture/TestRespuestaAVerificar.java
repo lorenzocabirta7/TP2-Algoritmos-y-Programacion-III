44264634658db02d960b41b02bdf25e489a1436d
ManuPuey <mapueyrredon@fi.uba.ar>
2024-07-01 19:35:05 -0300

    Agrego Test RespuestaAVerificar Viejos

--- src/main/test/edu/fiuba/algo3/architecture/TestRespuestaAVerificar.java
+++ src/main/test/edu/fiuba/algo3/architecture/TestRespuestaAVerificar.java
@@ -1,7 +1,7 @@
 package edu.fiuba.algo3.architecture;
 
+
 import edu.fiuba.algo3.modelo.Penalidad.Penalidad.PenalidadClasica;
-import edu.fiuba.algo3.modelo.Penalidad.Penalidad.TipoDePenalidad;
 import edu.fiuba.algo3.modelo.Respuestas.Respuesta;
 import edu.fiuba.algo3.modelo.Respuestas.RespuestaAVerificar;
 import edu.fiuba.algo3.modelo.Respuestas.RespuestaCorrecta;
@@ -13,5 +13,90 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestRespuestaAVerificar {
 
     @Test
-    public void test01RespuestaAVerificarIncorrectaDevuelveFalseCuandoSeLePreguntaSiEsCorrecta() {}
+    public void Test01UnaRespuestaAVerificarSeInicializaCorrectamente(){
+        String enunciadoEsperado = "enunciado De Prueba";
+        String ordenEsperado = "0";
+
+        Respuesta respuesta1 = new RespuestaAVerificar("enunciado De Prueba", "0");
+
+        String enunciadoObtenido = respuesta1.getRespuesta();
+        String ordenObtenido = respuesta1.getOrdenParcial();
+
+        assertEquals(enunciadoEsperado, enunciadoObtenido);
+        assertEquals(ordenEsperado, ordenObtenido);
+    }
+
+
+    @Test
+    public void Test02UnaRespuestaAVerificarSeActualizaAUnaRespuestaCorrectaDeFormaCorrecta(){
+
+        Boolean respuestaEsperada = true;
+
+        Respuesta respuestaCorrecta = new RespuestaCorrecta("enunciado De Prueba", "2");
+
+        Respuesta respuestaAVerificar = new RespuestaAVerificar("enunciado De Prueba", "2");
+
+        Boolean respuestaObtenida = respuestaAVerificar.EsCorrecta(respuestaCorrecta);
+
+        assertEquals(respuestaEsperada, respuestaObtenida);
+    }
+
+    @Test
+    public void Test03UnaRespuestaAVerificarSeActualizaAUnaRespuestaIncorrectaDeFormaCorrecta(){
+
+        Boolean respuestaEsperada = false;
+
+        Respuesta respuestaIncorrecta = new RespuestaCorrecta("enunciado De Prueba","1");
+
+        Respuesta respuestaAVerificar = new RespuestaAVerificar("enunciado De Prueba", "2");
+
+        Boolean respuestaObtenida = respuestaAVerificar.EsCorrecta(respuestaIncorrecta);
+
+        assertEquals(respuestaEsperada, respuestaObtenida);
+    }
+
+    @Test
+    public void Test04UnaRespuestaAVerificarActualizaElPuntajeDeFormaCorrectaCuandoRespondeBien(){
+
+        int puntajeEsperado = 1;
+
+        Respuesta respuestaCorrecta = new RespuestaCorrecta("enunciado De Prueba", "2");
+
+        Respuesta respuestaAVerificar = new RespuestaAVerificar("enunciado De Prueba", "2");
+
+        int puntajeObtenido = respuestaAVerificar.actualizarPuntaje(1, new PenalidadClasica(),respuestaCorrecta);
+
+        assertEquals(puntajeEsperado, puntajeObtenido);
+    }
+
+    @Test
+    public void Test05UnaRespuestaAVerificarActualizaElPuntajeDeFormaCorrectaCuandoRespondeMal(){
+
+        int puntajeEsperado = 0;
+
+        Respuesta respuestaCorrecta = new RespuestaCorrecta("enunciado De Prueba", "2");
+
+        Respuesta respuestaAVerificar = new RespuestaAVerificar("enunciado De Prueba", "3");
+
+        int puntajeObtenido = respuestaAVerificar.actualizarPuntaje(1, new PenalidadClasica(),respuestaCorrecta);
+
+        assertEquals(puntajeEsperado, puntajeObtenido);
+    }
+
+    @Test
+    public void Test05UnaRespuestaAVerificarActualizaElPuntajeDeFormaCorrectaCuandoRespondeBienAunqueHayanMasDe2RespuestasCorrectas(){
+
+        int puntajeEsperado = 1;
+
+        Respuesta respuestaCorrecta1 = new RespuestaCorrecta("enunciado De Prueba", "2");
+        Respuesta respuestaCorrecta2 = new RespuestaCorrecta("enunciado De Prueba2", "-3");
+
+        Respuesta respuestaAVerificar = new RespuestaAVerificar("enunciado De Prueba", "2");
+
+        int puntajeObtenido = respuestaAVerificar.actualizarPuntaje(1, new PenalidadClasica() ,respuestaCorrecta1);
+        puntajeObtenido += respuestaAVerificar.actualizarPuntaje(1, new PenalidadClasica() ,respuestaCorrecta2);
+
+
+        assertEquals(puntajeEsperado, puntajeObtenido);
+    }
 }
